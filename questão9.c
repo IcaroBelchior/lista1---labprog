c9117a04d45b17dd011c23ed183041f2b54184c4
IcaroBelchior <124515130+IcaroBelchior@users.noreply.github.com>
2023-02-15 09:05:20 -0300

    Create questão9.c

---
+++ questão9.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+   int x, y=1;
+   printf("Digite o valor de x: \n"); scanf("%d", &x);
+
+printf("Antecessor de x: %d \n", x-y);
+printf("Sucessor de x: %d \n", x+y);
+
+    return 0;
+}
