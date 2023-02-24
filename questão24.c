a9fb6583728da69c3d191c4ab6840daee1daec76
IcaroBelchior <124515130+IcaroBelchior@users.noreply.github.com>
2023-02-23 23:39:50 -0300

    Create questão24.c

---
+++ questão24.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    int x, n, i;
+    printf("Digite um valor para x: ");
+    scanf("%d", &x);
+    printf("Digite um valor para n: ");
+    scanf("%d", &n);
+    printf("o produto entre x por 2 elevado a n e: ", x * 2 ^ n);
+  
+    return 0;
+    
+}
