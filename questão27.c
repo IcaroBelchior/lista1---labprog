0d412f6db0691097cd4439b3cc0a5c355d6bfd92
IcaroBelchior <124515130+IcaroBelchior@users.noreply.github.com>
2023-02-23 23:43:28 -0300

    Create questão27.c

---
+++ questão27.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+    float num1, num2, num3, mediaAritmetica, mediaGeometrica;
+    printf("Digite o primeiro numero: "); scanf("%f", &num1);
+    printf("Digite o segundo numero: "); scanf("%f", &num2);
+    printf("Digite o terceiro numero: "); scanf("%f", &num3);
+  
+    mediaAritmetica = (num1 + num2 + num3) / 3;
+    mediaGeometrica = pow((num1 * num2 * num3), 1/3);
+  
+  
+    printf("Media Aritmetica: %f\nMedia Geometrica: %f\n", mediaAritmetica, mediaGeometrica);
+  
+  return 0;
+}
