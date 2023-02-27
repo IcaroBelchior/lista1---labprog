511abc02c011acd7ba64f0aaa28d18b16847a68f
IcaroBelchior <124515130+IcaroBelchior@users.noreply.github.com>
2023-02-27 09:06:24 -0300

    Update questão22.c

--- questão22.c
+++ questão22.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(){
     int i;
     char str[] = "ABCabc0123$*+/ ";
+
     for(i = 0; i < strlen(str); i++){
         printf("%d\n", str[i]);
     }
+
     return 0;
 }
