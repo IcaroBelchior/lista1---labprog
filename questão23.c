30a7a46a01b986a8c1e4302ee43acf0f1fef469c
IcaroBelchior <124515130+IcaroBelchior@users.noreply.github.com>
2023-02-27 09:11:12 -0300

    Update questão23.c

--- questão23.c
+++ questão23.c
@@ -2,12 +2,14 @@
 
 int main(){
     int num, num1, num2, num3;
-    printf("Digite um numero de tres digitos: ");
-    scanf("%d", &num);
+    printf("Digite um numero de tres digitos: ");scanf("%d", &num);
+
     num1 = num / 100;
     num2 = (num % 100) / 10;
     num3 = num % 10;
     num = num3 * 100 + num2 * 10 + num1;
-    printf("%d", num);
+
+    printf("%d\n", num);
+
     return 0;
 }
